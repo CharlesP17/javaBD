6fb679cb057f5505d297ce1a1d7ca504a6502363
CharlesP17 <charles.paradot@viacesi.fr>
2017-06-23 16:19:08 +0200

    o

--- boulderdash/main/src/main/java/main/Main.java
+++ boulderdash/main/src/main/java/main/Main.java
@@ -8,14 +8,14 @@ import view.Window;
 
 public class Main {
 
-private static Keyinput input;
+
 
     public static void main(final String[] args) 
     {
 
     	
     	Window fen = new Window();
-    	input = new Keyinput();
+    
     	
     	
     }}
--- boulderdash/view/src/main/java/view/Window.java
+++ boulderdash/view/src/main/java/view/Window.java
@@ -1,20 +1,51 @@
 package view;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.sql.SQLException;
+
 import javax.swing.JFrame;
 
-
-public class Window  {
-private static final long serialVersionIUD = 1L;
+/**
+ * <h1>The Class Window.</h1>
+ *
+ * @author Charles
+ * @version 1.0
+ */
+public class Window implements KeyListener  {
+	
+	/** Create the window*/
 		public Window()
 		{
 		JFrame windo = new JFrame();
-		windo.setTitle("BOULDER DASH TA MERE LA PUTE");
+		/** Title */
+		windo.setTitle("BOULDER DASH");
+		/** Size */
 		windo.setSize(336, 359);
+		/** location */
 		windo.setLocationRelativeTo(null);
+		/** CloseOperation */
 		windo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		/** ContentPane */
 		windo.setContentPane(new DisplayElements());
-		
+		/** visibility */
 		windo.setVisible(true);
+		}
+		@Override
+		public void keyPressed(KeyEvent e) {
+			int key = e.getKeyCode();
+		
+			
+		}
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
 		}
 
 
