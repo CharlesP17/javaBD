3aebdcc4ae7f9ca001fb367b4cad146f3fb83172
FlorentinC <cheraud.florentin@gmail.com>
2017-06-23 09:46:42 +0200

    add jdbc

---
+++ boulderdash/model/src/main/java/model/dao/Jdbc.java
@@ -0,0 +1,87 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * <h1>The Class Jdbc.</h1>
+ *
+ * @author Florentin
+ * @version 1.0
+ */
+
+public class Jdbc {
+
+	private static String map = null;
+
+	public static void main(String[] args) {
+		
+		readMapFromDB(); 
+		System.out.print(map);
+		
+	}
+	
+	/**
+     * Extract the map from the data
+     *
+     * @return the map on a string
+     * @throws SQLException
+     *             the SQL exception
+     */
+	
+	public static String readMapFromDB () {
+
+			
+	    
+		    /** The login. */
+		    String                  user     = "root";
+
+		    /** The password. */
+		    String                  password = "";
+
+		    /** The url. */
+		    String                  url      = "jdbc:mysql://localhost/boulderdash?useSSL=false&serverTimezone=UTC";
+
+		    /** The connection. */
+		    Connection                     cn = null;
+
+		    /** The statement. */
+		    Statement                      st = null;
+		    
+		    /** The ResultSet. */
+		    ResultSet rs = null;
+		    
+		    try {
+		    	Class.forName("com.mysql.jdbc.Driver");
+		    	cn = DriverManager.getConnection(url, user, password);
+		    	st = cn.createStatement();
+		    	String sql = "call GetMap(1)";
+		    	rs = st.executeQuery(sql);
+		    	while (rs.next())	{
+		    		//System.out.print(rs.getString("Map"));
+		    		map = rs.getString("Map");
+		    		
+		    	}
+		    }
+		    catch (SQLException e) {
+		    	e.printStackTrace();
+		    }
+		    catch (ClassNotFoundException e) {
+		    	e.printStackTrace();
+		    }
+		    finally {
+		    	try {
+		    		cn.close();
+		    		st.close();
+		    	}
+		    	catch (SQLException e) {
+		    		e.printStackTrace();
+		    	}
+		    }
+			return map;
+	}
+	
+}
